bd7b273c2b481f46355e3b545e23ed1d052d8a32
Taxrosdev <taxrosdev@mailbox.org>
2025-05-11 22:11:53 +1200

    Unfinished dracut

--- build.sh
+++ build.sh
@@ -13,5 +13,7 @@ bst build all.bst
 # Checkout the built artifact
 bst artifact checkout all.bst -f
 
+# This is generated from this script, and it does weird stuff if you forget to remove it.
+rm -rf mkosi.skeleton
 # Move all to mkosi.skeleton
 mv all mkosi.skeleton

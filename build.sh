fe06bf6bc6e2fa11a4817145a6290bd9fc726c16
Taxrosdev <taxrosdev@mailbox.org>
2025-05-13 01:35:09 +1200

    Slight reorginization, add base-devel (not complete)

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ rm -rf mkosi.skeleton
 rm -rf mkosi.images/initrd/mkosi.skeleton
 
 # Build TimelessOS Base & Artifacts
-bst build all.bst
-bst build initramfs.bst
+bst build builds/base.bst
+bst build builds/initramfs.bst
 
 # Checkout the built artifacts
-bst artifact checkout all.bst -f --directory mkosi.skeleton
-bst artifact checkout initramfs.bst -f --directory mkosi.images/initrd/mkosi.skeleton
+bst artifact checkout builds/base.bst -f --directory mkosi.images/base/mkosi.skeleton
+bst artifact checkout builds/initramfs.bst -f --directory mkosi.images/initrd/mkosi.skeleton

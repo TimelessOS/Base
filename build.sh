3308ad8f9d12ee03bda23dfec1c6f9d433c5c044
Taxrosdev <taxrosdev@mailbox.org>
2025-05-15 00:06:29 +1200

    SILLYISM

--- build.sh
+++ build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+# We need a better system at some point, but don't fix whats not broken
 
 set -eux
 
 # Prepare Platforms
-export PLATFORM=${PLATFORM:-amd}
+export PLATFORM=${PLATFORM:-amd-qemu}
 echo "Building for $PLATFORM"
 cp platforms/$PLATFORM/.config files/linux/.config
 
 rm -rf mkosi.images/base/mkosi.skeleton
 rm -rf mkosi.images/initrd/mkosi.skeleton
+rm -rf mkosi.images/gnome/mkosi.skeleton
 
 # Build TimelessOS Base & Artifacts
 bst build builds/base.bst
 bst build builds/initramfs.bst
+bst build builds/gnome.bst
 
 # Checkout the built artifacts
 bst artifact checkout builds/base.bst -f --directory mkosi.images/base/mkosi.skeleton
 bst artifact checkout builds/initramfs.bst -f --directory mkosi.images/initrd/mkosi.skeleton
+bst artifact checkout builds/gnome.bst -f --directory mkosi.images/gnome/mkosi.skeleton
  
